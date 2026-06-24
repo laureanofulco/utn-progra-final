d085bce45f71d0c7deb417397c760b58ee2fccd6
Laureano Fulco <14047619+laureanofulco@users.noreply.github.com>
2026-06-23 21:08:33 -0300

    Refactorización de buscar_admin

--- funciones/logica.c
+++ funciones/logica.c
@@ -46,8 +46,54 @@ void generar_archivo_admins(void)
 
     mensaje("OK", "Administrador creado correctamente");
 
-    getchar();
+    pausarf();
 }
+
+/**
+ * @brief Busca un administrador por nombre.
+ *
+ * Recorre el archivo de administradores buscando una coincidencia
+ * con el nombre indicado. Si se encuentra y el parámetro admin
+ * no es NULL, copia los datos encontrados en dicha estructura.
+ *
+ * @param nombre Nombre del administrador a buscar.
+ * @param admin Puntero donde se almacenará el administrador encontrado.
+ * @return 1 si el administrador existe, 0 en caso contrario.
+ */
+int buscar_admin(char nombre[], Administrador* admin)
+{
+	FILE* archivo = fopen(ARCHIVO_ADMINS, "rb");
+
+	Administrador admin_aux;
+
+	int encontrado = 0;
+	
+	if(archivo != NULL)
+    {
+		while(fread(&admin_aux, sizeof(Administrador), 1, archivo) > 0)
+        {
+			if(strcmp(admin_aux.nombre, nombre) == 0)
+            {
+				encontrado = 1;
+
+				if(admin != NULL)
+                {
+					*admin = admin_aux;
+				}
+
+				break;
+			}
+		}
+
+		fclose(archivo);
+	} else
+    {
+		mensaje("ERROR", "No se pudo abrir el archivo");
+	}
+
+	return encontrado;
+}
+
 
 /**
  * @brief Registra un nuevo administrador.
@@ -55,9 +101,6 @@ void generar_archivo_admins(void)
  * Solicita nombre y contraseña, verifica que el administrador
  * no exista previamente y almacena los datos en el archivo.
  * La contraseña es cifrada antes de ser guardada.
- *
- * @see buscar_admin()
- * @see cifrar()
  */
 void alta_admin(void)
 {
@@ -65,11 +108,11 @@ void alta_admin(void)
 	
 	printf("Ingrese nombre: ");
 	scanString(admin.nombre, 50);
-	convertir_minuscula(admin.nombre);
 	
-	if(buscar_admin(admin.nombre, NULL) == 1){
+	if(buscar_admin(admin.nombre, NULL) == 1)
+    {
 		mensaje("ERROR", "Admin existente");
-		getchar();
+		pausarf();
 		return;
 	}
 		
--- funciones/logica.h
+++ funciones/logica.h
@@ -2,6 +2,7 @@
 #define LOGICA_H
 
 void generar_archivo_admins(void);
+int buscar_admin(char nombre[], Administrador *admin);
 void alta_admin(void);
 
 
